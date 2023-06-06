17876079145bcf1aa28f8c1dc127c6f14eda0e6d
Dimitris Petrou <petroudimitris@outlook.com>
2023-06-06 21:20:52 +0300

    Made corrections to sigma mistakes

--- ExecutableService/core/src/main/java/org/example/Merger.java
+++ ExecutableService/core/src/main/java/org/example/Merger.java
@@ -1,4 +1,4 @@
-package org.exaple;
+package org.example;
 
 import java.io.File;
 import java.io.FileWriter;
--- ExecutableService/core/src/main/java/org/example/Shuffler.java
+++ ExecutableService/core/src/main/java/org/example/Shuffler.java
@@ -1,4 +1,4 @@
-package model;
+package org.example;
 
 import java.io.File;
 import java.io.FileWriter;
