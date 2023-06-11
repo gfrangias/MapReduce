8077eb237bfca0b43cd8f8ced198eb579c3bd76f
Dimitris Petrou <petroudimitris@outlook.com>
2023-06-11 10:35:01 +0300

    Made distributor iterative for multiple chunks per worker

--- ExecutableService/core/src/main/java/org/example/Distributor.java
+++ ExecutableService/core/src/main/java/org/example/Distributor.java
@@ -6,47 +6,52 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 public class Distributor {
 	
-	public Distributor(File file, long chunkSize, long id) {
+	public Distributor(File file, long chunkSize, long offset, int numOfChunks) {
 		this.inputFile = file;
-		CHUNK_SIZE = chunkSize;
-		ID = id;
+		this.numOfChunks = numOfChunks;
+		this.CHUNK_SIZE = chunkSize;
+		this.offset = offset;
 	}
 	
 	public final File inputFile;
 	public final long CHUNK_SIZE;
-	public final long ID;
+	public final long offset;
+	public final int numOfChunks;
 	
 	public boolean create_chunk_file() throws IOException {
-		
-		RandomAccessFile stream = new RandomAccessFile(inputFile, "r");
-		
-		stream.seek(CHUNK_SIZE * ID);
-		
-		byte[] data = new byte[(int) CHUNK_SIZE];
-		
-		stream.read(data);
-		stream.close();
-		
-		File outputFile = new File("chunked_" + ID + ".intermediate");
-		FileOutputStream fos = new FileOutputStream(outputFile);
-		
-		fos.write(data);;
-		fos.close();
-		
+
+		for (int i=0; i<numOfChunks;i++){
+			RandomAccessFile stream = new RandomAccessFile(inputFile, "r");
+
+			stream.seek(this.offset+(i*CHUNK_SIZE));
+
+			byte[] data = new byte[(int) CHUNK_SIZE];
+
+			stream.read(data);
+			stream.close();
+
+			File outputFile = new File("chunked_" + ID + ".intermediate");
+			FileOutputStream fos = new FileOutputStream(outputFile);
+
+			fos.write(data);
+			fos.close();
+		}
+
 		return true;
 	}
 	
 	public static void main(String[] args) throws IOException, IllegalArgumentException {
 		
-		if (args.length != 3) {
+		if (args.length != 4) {
 			throw new IllegalArgumentException("Insufficient Arguments");
 		}
 		
 		String fileName = args[0];
 		int chunk_size = Integer.parseInt(args[1]);
-		int id = Integer.parseInt(args[2]);
+		long offset = Integer.parseInt(args[2]);
+		int numOfCh = Long.parseLong(args[3]);
 		
-		Distributor chunk = new Distributor(new File(fileName), chunk_size, id);
+		Distributor chunk = new Distributor(new File(fileName), chunk_size, offset, numOfCh);
 		
 		boolean status = chunk.create_chunk_file();
 		
