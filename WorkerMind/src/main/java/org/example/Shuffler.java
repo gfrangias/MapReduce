6f309ef94852c416f8afe6c3ede54fc53e636901
Dimitris Petrou <petroudimitris@outlook.com>
2023-06-24 23:04:29 +0300

    Shuffler works correctly and produces intermediate files in appropriate job dir

--- WorkerMind/src/main/java/org/example/Shuffler.java
+++ WorkerMind/src/main/java/org/example/Shuffler.java
@@ -1,58 +1,96 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
+import java.io.*;
+import java.util.*;
 
 public class Shuffler {
-
+	
 	public static void shuffle(String[] files, int numOfReduces, String outputPath) throws IOException {
-
+		
 		int reducers = numOfReduces;
 
 		HashMap<String, ArrayList<Integer>> map = new HashMap<String, ArrayList<Integer>>();
 
-		for (int i=1; i< files.length; i++) {
+		for (int i=0; i< files.length; i++) {
 			String filename = files[i];
 			System.out.println("Accessing file: "+filename);
-			Scanner stream = new Scanner(new File(filename));
 
-			String key_object = null;
-			String value_object = null;
+			File map_file = new File(filename);
 
-			String line = null;
+			if (map_file.exists() == false) {
+				System.out.println("File does not exist...");
+				System.exit(0);
+			}
+			
+			RandomAccessFile stream = new RandomAccessFile(map_file, "r");
+			
+			long position = 0;
+			
+			main_loop: while (true) {
+				
+				System.out.println(position);
 
-
-			while (true) {
-				try {
-					line = stream.nextLine();
-				} catch (NoSuchElementException e) {
+				byte[] data = new byte[1 << 26];
+				
+				if (stream.read(data) == -1) {
 					break;
 				}
 
-				String[] line_elements = line.split(" ");
+				long key_from_position = 0;
+				long key_to_position = 0;
 
-				if (line_elements.length != 2) {
-					stream.close();
-					throw new IllegalStateException("Each line should contain a key-value pair");
+				long value_from_position = 0;
+				long value_to_position = 0;
+
+				long last_newline = 0;
+
+				for (int j=0; j<data.length; j++) {
+					if (data[j] == 0) {
+						break main_loop;
+					}
+
+					if (data[j] == 0x20) {
+						key_to_position = j;
+						value_from_position = j+1;
+					}
+
+					if (data[j] == 0x0A) {
+
+						last_newline = j;
+
+						value_to_position = j;
+
+						byte[] k = Arrays.copyOfRange(data, (int) key_from_position, (int) key_to_position);
+						String key_str = new String(k);
+
+						byte[] v = Arrays.copyOfRange(data, (int) value_from_position, (int) value_to_position);
+						String value_str = new String(v);
+
+						key_from_position = j+1;
+
+						if (!map.containsKey(key_str)) {
+							map.put(key_str, new ArrayList<Integer>(1000));
+						}
+						else {
+							map.get(key_str).add(Integer.parseInt(value_str));
+						}
+
+					}
+
+					if (data[j] == 0) {
+						break;
+					}
+
 				}
 
-				key_object = line_elements[0];
-				value_object = line_elements[1];
-
-				if (!map.containsKey(key_object)) {
-					map.put(key_object, new ArrayList<Integer>(1000));
-				}
-
-				map.get(key_object).add(Integer.parseInt(value_object));
+				position += last_newline + 1;
+				
+				stream.seek(position);	
 			}
-
+			
+			stream.close();
 		}
-
+		
 		ArrayList<String> keys = new ArrayList<String>(map.keySet());
 
 		ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>(reducers);
@@ -81,5 +119,7 @@ public class Shuffler {
 
 			stream.close();
 		}
+		
 	}
+
 }
